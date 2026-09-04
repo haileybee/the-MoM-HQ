2752a63e6f0321bb9ad73b6e4081f14bed068514
haileybee <53802118+haileybee@users.noreply.github.com>
2026-09-03 21:34:28 -0600

    Connect MoM HQ PayPal donation link

--- momhq-config.js
+++ momhq-config.js
@@ -1,5 +1,5 @@
 window.MOMHQ_CONFIG = Object.freeze({
   supabaseUrl: 'https://zljduaahbyxnuglbugar.supabase.co',
   supabaseKey: 'sb_publishable_ZljzdOZd1LDQ5LX8z-5tNg_2SfCJGFc',
-  paypalDonationUrl: ''
+  paypalDonationUrl: 'https://www.paypal.com/donate/?hosted_button_id=YEKVQUST4LQK2'
 });
