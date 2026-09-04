096134e302ce56adbb5e2ac4e8a785c7565db256
haileybee <53802118+haileybee@users.noreply.github.com>
2026-09-03 18:58:43 -0600

    Add public MoM HQ web configuration

---
+++ momhq-config.js
@@ -0,0 +1,5 @@
+window.MOMHQ_CONFIG = Object.freeze({
+  supabaseUrl: 'https://zljduaahbyxnuglbugar.supabase.co',
+  supabaseKey: 'sb_publishable_ZljzdOZd1LDQ5LX8z-5tNg_2SfCJGFc',
+  paypalDonationUrl: ''
+});
